d43cbc2e3851f61fc1491cdf3e1fb07b9cb3d6d4
Josed1054 <61340308+Josed1054@users.noreply.github.com>
2022-07-31 15:13:32 -0500

    fixed some styles

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -3,9 +3,9 @@ import "./style.css";
 
 export function App() {
   return (
-    <>
+    <div className="Home">
       <h1>Welcome To Personas DB</h1>
       <h1>⚛</h1>
-    </>
+    </div>
   );
 }
